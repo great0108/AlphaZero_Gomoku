a5979fbb2c05f921437f1a447fc986728703787e
great0108 <onlybesthome@gmail.com>
2023-11-07 13:20:46 +0900

    human play

--- test.py
+++ test.py
@@ -4,22 +4,21 @@ import numpy as np
 # a[0] = (1,2)
 # print(a)
 
-import sys
-input=sys.stdin.readline
-L=[0.500, 0.333, 0.090]
-a=len(L)
-for count in range(1,1000) :
-    possible=0
-    for i in range(a) :
-        if L[i]==0 :
-            possible+=1
-            continue
-        if L[i]*count>=1 :
-            if L[i]*count<=round(L[i]*count) and ((L[i]+0.0009)*count)>=round((L[i]+0.0009)*count) and round(L[i]*count)==round((L[i]+0.0009)*count) :
-                possible+=1
-    if possible==a :
-        print(count)
-        break
+# import sys
+# input=sys.stdin.readline
+# L=[0.500, 0.333, 0.090]
+# a=len(L)
+# for count in range(1,1000) :
+#     possible=0
+#     for i in range(a) :
+#         if L[i]==0 :
+#             possible+=1
+#             continue
+#         if L[i]*count>=1 :
+#             if L[i]*count<=round(L[i]*count) and ((L[i]+0.0009)*count)>=round((L[i]+0.0009)*count) and round(L[i]*count)==round((L[i]+0.0009)*count) :
+#                 possible+=1
+#     if possible==a :
+#         print(count)
+#         break
 
-# for i in range(1, 100):
-#     print(1/i)
+print(100 * "준")
