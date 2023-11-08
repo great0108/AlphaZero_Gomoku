caa1212ca64edeeab4b27425eff9aaef522b0a74
master <onlybesthome@gmail.com>
2023-11-08 14:45:32 +0900

    back

--- train.py
+++ train.py
@@ -92,16 +92,16 @@ class TrainPipeline():
             for i in [1, 2, 3, 4]:
                 # rotate counterclockwise
                 equi_state = np.array([np.rot90(s, i) for s in state])
-                equi_mcts_prob = np.rot90(
-                    mcts_porb.reshape(state.shape[1], state.shape[2]), i)
+                equi_mcts_prob = np.rot90(np.flipud(
+                    mcts_porb.reshape(self.board_height, self.board_width)), i)
                 extend_data.append((equi_state,
-                                    equi_mcts_prob.flatten(),
+                                    np.flipud(equi_mcts_prob).flatten(),
                                     winner))
                 # flip horizontally
                 equi_state = np.array([np.fliplr(s) for s in equi_state])
                 equi_mcts_prob = np.fliplr(equi_mcts_prob)
                 extend_data.append((equi_state,
-                                    equi_mcts_prob.flatten(),
+                                    np.flipud(equi_mcts_prob).flatten(),
                                     winner))
         return extend_data
 
